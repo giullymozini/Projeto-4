ee68db1f2d1cc504903b8aa0a7155c72e29d65d0
giuliana <giulianalucaslima@gmail.com>
2024-04-01 09:00:32 -0300

    Criando o script do calculo

---
+++ script.js
@@ -0,0 +1,7 @@
+function calc(){
+    //declarando as variaveis 
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+
+    document.getElementById('resultado').innerHTML = num1 * num2
+}
